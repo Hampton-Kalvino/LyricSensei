74b14a210694e90a0816b32d8a88494d371ca4a9
Kelvin Roundtree <kelvinroundtree@live.com>
2025-12-05 16:29:01 -0600

    updated main activity

--- android/app/src/main/java/com/lyricsensei/app/MainActivity.java
+++ android/app/src/main/java/com/lyricsensei/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.lyricsensei.app;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Base64;
+import java.security.MessageDigest;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.lyricsensei.app",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String keyHash = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.d("KeyHash", "Key Hash: " + keyHash);
+            }
+        } catch (Exception e) {
+            Log.e("KeyHash", "Error getting key hash", e);
+        }
+    }
+}
